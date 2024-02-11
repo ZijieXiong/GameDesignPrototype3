98464c3d96a269fcdb327014b2ca197aa0dfe587
Zijie Xiong <zx811@nyu.edu>
2024-02-10 21:11:45 -0500

    Add player movement and colliders

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5.0f;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        Vector2 newPosition = rb.position + new Vector2(moveX, moveY) * speed * Time.fixedDeltaTime;
+        
+        rb.MovePosition(newPosition);
+    }
+    
+    void OnCollisionEnter2D(Collision2D collision)
+{
+    if (collision.gameObject.CompareTag("Obstacles"))
+    {
+        Debug.Log("Player has collided with an enemy!");
+    }
+}
+}
